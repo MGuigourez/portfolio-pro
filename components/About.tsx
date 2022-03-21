608f35808477a8ebfe98e979199be182e267a60b
= <=>
2022-03-21 23:30:04 +0100

    fin jour 1 : hover url c&v dark / white mode

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import profile from '../public/profil.jpg'
 
+import { useTheme } from 'next-themes'
+
 const About = () => {
+  const { theme, setTheme, resolvedTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), [])
+
   return (
     <div className="container mx-auto px-4">
       <div className="item-center flex flex-col-reverse text-center lg:-mx-4 lg:flex lg:flex-row lg:space-x-5 lg:text-left">
@@ -13,11 +22,24 @@ const About = () => {
           <div className="mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
               Je m'appelle Maxime Guigourez, je suis actuellement développeur
-              chez
-              <a href="https://checkandvisit.com/" target="_blank">
-                {' '}
-                Check & Visit{' '}
-              </a>{' '}
+              chez{' '}
+              {mounted && (theme === 'dark' || resolvedTheme === 'dark') ? (
+                <a
+                  href="https://checkandvisit.com/"
+                  target="_blank"
+                  className="text-blue-600  visited:text-purple-600 hover:bg-red-800"
+                >
+                  Check & Visit{' '}
+                </a>
+              ) : (
+                <a
+                  href="https://checkandvisit.com/"
+                  target="_blank"
+                  className="text-blue-600 visited:text-purple-600  hover:bg-green-300"
+                >
+                  Check & Visit{' '}
+                </a>
+              )}{' '}
               où j'effectue mon alternance depuis bientôt 2 ans en vue de
               l'obtention d'un Master "Expert en informatique et Système
               d'information" en fin d'année 2022.
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="mx-auto max-w-4xl px-4 text-gray-800 dark:text-white">
         <div className="dark:border-white-300 mb-2 border-t-2 border-gray-300 pb-8"></div>
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <p>Built with Next.js, Tailwind and Vercel</p>
+          <p>Réalisé avec Next.js, Tailwind et Vercel</p>
           <div className="flex flex-wrap space-x-2 pt-2 font-medium sm:space-x-4 lg:pt-0">
             <a
               href="https://twitter.com/MGuigourez"
